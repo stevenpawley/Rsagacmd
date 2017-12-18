1f3346adc04d7f9c3dbd3f6c125aab102efabb8b
Steve <dr.stevenpawley@gmail.com>
2017-12-18 00:56:17 -1000

    Added initial package help

--- R/Rsagacmd.R
+++ R/Rsagacmd.R
@@ -6,6 +6,29 @@ library(rgdal)
 library(foreign)
 library(sf)
 
+#' Rsagacmd: A package for linking R with the open-source SAGA-GIS.
+#'
+#' Rsagacmd is intended to provide an R-like scripting environment to the open-source SAGA-GIS.
+#' The current version has only been tested using SAGA-GIS 5.0.0 and 6.1.0 on Windows (x64),
+#' OS X and Linux.
+#' 
+#' This package is not related to the RSAGA package, which provides an excellent connection
+#' to SAGA-GIS versions 2.0.4 - 2.2.3. However, in addition to supporting newer versions of
+#' SAGA-GIS, Rsagacmd provides a more functional method of accessing SAGA-GIS tools and
+#' libraries. Rsagacmd works by dynamically generating R functions for every SAGA-GIS tool
+#' and embeds these functions within a nested list structure. This facilitates an easier
+#' scripting experience because the function's syntax are similar to using the SAGA-GIS
+#' command line tool directly, and the user can also take advantage of code autocompletion
+#' tools (e.g in Rstudio), allowing for each tools' inputs, outputs and options to be more
+#' easily recognized.
+#' 
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name Rsagacmd
+NULL
+
 
 #' Function to return the installed version of SAGA-GIS
 #'
