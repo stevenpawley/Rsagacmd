f87660ae6de2359bba7750035b7e2d85a46ee4d4
Steve <dr.stevenpawley@gmail.com>
2018-10-09 09:53:23 -0600

    Included example elevation tile and use data within examples

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example elevation data
+#'
+#' A small tile of elevation data derived from the open-source
+#' GRASS-GIS 7.x example North Carolina dataset
+#'
+#' @format A RasterLayer object with 75 rows and 70 columns. For more details
+#' refer to the GRASS GIS North Carolina sample data:
+#' \describe{
+#'   \item{elevation}{Altitude in masl.}
+#'   ...
+#' }
+#' @source \url{https://grass.osgeo.org/download/sample-data}
+"elevation"
