0db9f979d16f3385f9fb0b24a59a030f6ee03a70
stevenpawley <dr.stevenpawley@gmail.com>
2019-10-25 10:40:46 -0600

    Separate saga version tests into separate test cases

--- tests/testthat/test-supported-saga-versions.R
+++ tests/testthat/test-supported-saga-versions.R
@@ -35,37 +35,155 @@ download_saga <- function(surl) {
 }
 
 
-testthat::test_that("initiation of all supported SAGA-GIS versions", {
+testthat::test_that("initiation of SAGA-GIS 2.3.1", {
   testthat::skip_on_cran()
+  testthat::skip_if_not(
+    Sys.info()["sysname"] == "Windows" & 
+    Sys.info()["machine"] == "x86-64")
+  
+  saga_url <-
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%202/SAGA%202.3.1/saga_2.3.1_x64.zip"
+  saga_bin <- download_saga(saga_url)
+  
+  if (!is.null(saga_bin)) {
+    saga <- saga_gis(saga_bin)
+    testthat::expect_false(is.null(saga))
+    testthat::expect_gt(length(saga), 0)
+    testthat::expect_is(
+      saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1),
+      "RasterLayer")
+    unlink(dirname(saga_bin), recursive = TRUE)
+  }
+})
 
-  if (Sys.info()["sysname"] == "Windows" & Sys.info()["machine"] == "x86-64") {
-    saga_urls <- c(
-      "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%202/SAGA%202.3.1/saga_2.3.1_x64.zip",
-      #' https://sourceforge.net/projects/saga-gis/files/SAGA%20-%203/SAGA%20-%203.0.0/saga_3.0.0_x64.zip',
-      "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%204/SAGA%20-%204.0.0/saga_4.0.0_x64.zip",
-      #' https://sourceforge.net/projects/saga-gis/files/SAGA%20-%204/SAGA%20-%204.0.1/saga-4.0.1_x64.zip',
-      #' https://sourceforge.net/projects/saga-gis/files/SAGA%20-%204/SAGA%20-%204.1.0/saga-4.1.0_x64.zip',
-      "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%205/SAGA%20-%205.0.0/saga-5.0.0_x64.zip",
-      "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%206/SAGA%20-%206.0.0/saga-6.0.0_x64.zip",
-      #' https://sourceforge.net/projects/saga-gis/files/SAGA%20-%206/SAGA%20-%206.1.0/saga-6.1.0_x64.zip',
-      #' https://sourceforge.net/projects/saga-gis/files/SAGA%20-%206/SAGA%20-%206.2.0/saga-6.2.0_x64.zip',
-      #' https://sourceforge.net/projects/saga-gis/files/SAGA%20-%206/SAGA%20-%206.3.0/saga-6.3.0_x64.zip',
-      #' https://sourceforge.net/projects/saga-gis/files/SAGA%20-%206/SAGA%20-%206.4.0/saga-6.4.0_x64.zip',
-      "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%207/SAGA%20-%207.0.0/saga-7.0.0_x64.zip"
-    )
 
-    lapply(saga_urls, function(surl) {
-      saga_bin <- download_saga(surl)
+testthat::test_that("initiation of SAGA-GIS 3.0.0", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(
+    Sys.info()["sysname"] == "Windows" & 
+      Sys.info()["machine"] == "x86-64")
 
-      if (!is.null(saga_bin)) {
-        saga <- saga_gis(saga_bin)
-        testthat::expect_false(is.null(saga))
-        testthat::expect_gt(length(saga), 0)
-        testthat::expect_is(
-          saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1), 
-          "RasterLayer")
-        unlink(dirname(saga_bin), recursive = TRUE)
-      }
-    })
+  saga_url <-
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%203/SAGA%20-%203.0.0/saga_3.0.0_x64.zip"
+  saga_bin <- download_saga(saga_url)
+  
+  if (!is.null(saga_bin)) {
+    saga <- saga_gis(saga_bin)
+    testthat::expect_false(is.null(saga))
+    testthat::expect_gt(length(saga), 0)
+    testthat::expect_is(
+      saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1),
+      "RasterLayer")
+    unlink(dirname(saga_bin), recursive = TRUE)
+  }
+})
+
+
+testthat::test_that("initiation of SAGA-GIS 4.0.0", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(
+    Sys.info()["sysname"] == "Windows" & 
+      Sys.info()["machine"] == "x86-64")
+    
+  saga_url <-
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%204/SAGA%20-%204.0.0/saga_4.0.0_x64.zip"
+  saga_bin <- download_saga(saga_url)
+  
+  if (!is.null(saga_bin)) {
+    saga <- saga_gis(saga_bin)
+    testthat::expect_false(is.null(saga))
+    testthat::expect_gt(length(saga), 0)
+    testthat::expect_is(
+      saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1),
+      "RasterLayer")
+    unlink(dirname(saga_bin), recursive = TRUE)
+  }
+})
+
+
+testthat::test_that("initiation of SAGA-GIS 5.0.0", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(
+    Sys.info()["sysname"] == "Windows" & 
+      Sys.info()["machine"] == "x86-64")
+    
+  saga_url <-
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%205/SAGA%20-%205.0.0/saga-5.0.0_x64.zip"
+  saga_bin <- download_saga(saga_url)
+  
+  if (!is.null(saga_bin)) {
+    saga <- saga_gis(saga_bin)
+    testthat::expect_false(is.null(saga))
+    testthat::expect_gt(length(saga), 0)
+    testthat::expect_is(
+      saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1),
+      "RasterLayer")
+    unlink(dirname(saga_bin), recursive = TRUE)
+  }
+})
+
+
+testthat::test_that("initiation of SAGA-GIS 6.0.0", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(
+    Sys.info()["sysname"] == "Windows" & 
+      Sys.info()["machine"] == "x86-64")
+  
+  saga_url <-
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%206/SAGA%20-%206.0.0/saga-6.0.0_x64.zip"
+  saga_bin <- download_saga(saga_url)
+  
+  if (!is.null(saga_bin)) {
+    saga <- saga_gis(saga_bin)
+    testthat::expect_false(is.null(saga))
+    testthat::expect_gt(length(saga), 0)
+    testthat::expect_is(
+      saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1),
+      "RasterLayer")
+    unlink(dirname(saga_bin), recursive = TRUE)
+  }
+})
+
+
+testthat::test_that("initiation of SAGA-GIS 7.0.0", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(
+    Sys.info()["sysname"] == "Windows" & 
+      Sys.info()["machine"] == "x86-64")
+  
+  saga_url <-
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%207/SAGA%20-%207.0.0/saga-7.0.0_x64.zip"
+  saga_bin <- download_saga(saga_url)
+  
+  if (!is.null(saga_bin)) {
+    saga <- saga_gis(saga_bin)
+    testthat::expect_false(is.null(saga))
+    testthat::expect_gt(length(saga), 0)
+    testthat::expect_is(
+      saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1),
+      "RasterLayer")
+    unlink(dirname(saga_bin), recursive = TRUE)
+  }
+})
+
+
+testthat::test_that("initiation of SAGA-GIS 7.4.0", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not(
+    Sys.info()["sysname"] == "Windows" & 
+      Sys.info()["machine"] == "x86-64")
+  
+  saga_url <-
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%207/SAGA%20-%207.4.0/saga-7.4.0_x64.zip"
+  saga_bin <- download_saga(saga_url)
+  
+  if (!is.null(saga_bin)) {
+    saga <- saga_gis(saga_bin)
+    testthat::expect_false(is.null(saga))
+    testthat::expect_gt(length(saga), 0)
+    testthat::expect_is(
+      saga$grid_calculus$random_terrain(ITERATIONS = 1, RADIUS = 1),
+      "RasterLayer")
+    unlink(dirname(saga_bin), recursive = TRUE)
   }
 })
