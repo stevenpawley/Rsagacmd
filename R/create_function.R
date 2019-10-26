13a73abe7ad2b6f054f5663bee380c12e64662c9
stevenpawley <dr.stevenpawley@gmail.com>
2019-10-25 18:27:45 -0600

    Move dynamic function body into separate file

---
+++ R/create_function.R
@@ -0,0 +1,20 @@
+create_function <- function(lib, tool) {
+  paste(
+    paste0("args = as.list(environment())"),
+    paste0("lib = ", deparse(lib)),
+    paste0("tool = ", deparse(tool)),
+    "
+        # remove intern and help from saga args list
+        if ('intern' %in% names(args))
+            args = args[-which(names(args) == 'intern')]
+        
+        if ('all_outputs' %in% names(args))
+            args = args[-which(names(args) == 'all_outputs')]
+        
+        # call the saga geoprocessor
+        saga_results = saga_execute(lib, tool, senv, intern, all_outputs, args)
+        return (saga_results)
+        ",
+    sep = "\n"
+  )
+}
--- R/saga.R
+++ R/saga.R
@@ -318,30 +318,11 @@ saga_gis <- function(saga_bin = NULL, grid_caching = FALSE,
       
       args <- tool_options %>% 
         sapply(function(x) x$alias, USE.NAMES = FALSE) %>%
-        paste0("=NULL", collapse = ", ")
-      
-      # define function body
-      # deparse tool settings into function and create code to call saga_execute
-      body <- paste(
-        paste0("args = as.list(environment())"),
-        paste0("lib = ", deparse(lib)),
-        paste0("tool = ", deparse(tool)),
-        "
-        # remove intern and help from saga args list
-        if ('intern' %in% names(args))
-            args = args[-which(names(args) == 'intern')]
-        
-        if ('all_outputs' %in% names(args))
-            args = args[-which(names(args) == 'all_outputs')]
-        
-        # call the saga geoprocessor
-        saga_results = saga_execute(lib, tool, senv, intern, all_outputs, args)
-        return (saga_results)
-        ",
-        sep = "\n"
-      )
+        paste0(" = NULL", collapse = ", ")
       
       # parse function
+      body <- create_function(lib, tool)
+      
       tryCatch(
         expr = {
           func_code <- paste0(
